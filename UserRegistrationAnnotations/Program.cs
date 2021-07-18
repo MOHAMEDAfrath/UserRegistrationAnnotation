37fee0b99d2cb499145ecdf6816b91d95b614a20
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-18 12:37:27 +0530

    [Afrath] Welcome to User Registration using Annotations

---
+++ UserRegistrationAnnotations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
